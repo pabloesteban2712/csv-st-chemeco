7d05e6dc8865501aff58eeb725f1de14cec757c8
Pablo <estebangagopablo7@gmail.com>
2025-05-07 11:47:54 +0100

    Fixing

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 import streamlit as st
 import pandas as pd
 
+st.title ("ChemECO - CSV Visuals")
+
 uploaded_file = st.file_uploader("chemeco.csv", type="csv")
 
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    try:
+        df = pd.read_csv(uploaded_file, encoding='utf-8', sep=None, engine='python')
+        st.write("Datos cargados:", df)
 
-    st.write("Loaded data: ", df)
+        if st.checkbox("Mostrar estadísticas descriptivas"):
+            st.write(df.describe())
 
-    if st.checkbox  ("Show described stats"):
-        st.write(df.describe())
-
-
+    except Exception as e:
+        st.error(f"Error al leer el archivo CSV: {e}")
     
 
     
